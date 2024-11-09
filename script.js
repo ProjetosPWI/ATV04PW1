ff1e5c6b728b0136232303d88a5186b912faac10
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2024-11-08 22:23:26 -0300

    add: script.js
    Início da Atividade em Aula

---
+++ script.js
@@ -0,0 +1,19 @@
+// Requisitos
+// 1
+const livros = ['Harry Potter', 'Senhor dos Aneis', 'Persy Jackson'];
+  
+// 2
+const autores = new Map([
+    ['Harry Potter', 'J. K. Rowling'],
+    ['Senhor dos Aneis', 'J. R. R. Tolkien'],
+    ['Persy Jackson', 'Rick Riordan']
+]);
+
+// 3
+const generos = new Set(['Aventura', 'Ação', 'Fantasia']);
+
+// 4
+function buscarLivrosPorGenero() 
+{
+
+}
